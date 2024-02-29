d3106cde51506d329a31bb3d4bef4ad65b10d7fd
hong-sile <hyukjoon.dev@gmail.com>
2024-02-29 23:20:50 +0900

    init project

---
+++ src/main/java/capstone/safeat/SafeatApplication.java
@@ -0,0 +1,13 @@
+package capstone.safeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SafeatApplication.class, args);
+  }
+
+}
---
+++ src/test/java/capstone/safeat/SafeatApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.safeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeatApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

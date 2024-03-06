3acccf7a040e34e46459b36c33c4f39e1ce88b62
hong-sile <hyukjoon.dev@gmail.com>
2024-03-06 23:22:00 +0900

    feat: GoogleMemberClient 정의
    - AccessToken 받는 기능까지 구현

---
+++ src/main/java/capstone/safeat/oauth/domain/GoogleMemberClient.java
@@ -0,0 +1,39 @@
+package capstone.safeat.oauth.domain;
+
+import static capstone.safeat.oauth.domain.OAuthServerType.GOOGLE;
+
+import capstone.safeat.oauth.GoogleOAuthConfig;
+import capstone.safeat.oauth.external.GoogleAccessToken;
+import capstone.safeat.oauth.external.GoogleApiClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Component
+@RequiredArgsConstructor
+public class GoogleMemberClient implements OAuthMemberClient {
+
+  private final GoogleApiClient googleApiClient;
+  private final GoogleOAuthConfig googleOAuthConfig;
+
+  @Override
+  public OAuthMemberInfo fetchMember(final String authCode) {
+    final GoogleAccessToken accessToken = googleApiClient.fetchToken(createRequestParam(authCode));
+    return null;
+  }
+
+  private MultiValueMap<String, String> createRequestParam(final String authCode) {
+    final MultiValueMap<String, String> param = new LinkedMultiValueMap<>();
+    param.add("grant_type", "authorization_code");
+    param.add("code", authCode);
+    param.add("redirect_uri", googleOAuthConfig.redirectUri());
+    param.add("client_id", googleOAuthConfig.clientId());
+    return param;
+  }
+
+  @Override
+  public OAuthServerType supportType() {
+    return GOOGLE;
+  }
+}

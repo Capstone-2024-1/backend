e58bf90f044dbb85d2ab23d71595725e9805b1c1
hong-sile <hyukjoon.dev@gmail.com>
2024-03-10 16:36:43 +0900

    feat: Google AccessToken으로 memberProfile을 조회하는 기능 구현
    #1

--- src/main/java/capstone/safeat/oauth/external/GoogleApiClient.java
+++ src/main/java/capstone/safeat/oauth/external/GoogleApiClient.java
@@ -1,13 +1,21 @@
 package capstone.safeat.oauth.external;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
 public interface GoogleApiClient {
 
   @PostExchange(url = "https://oauth2.googleapis.com/token", contentType = APPLICATION_FORM_URLENCODED_VALUE)
   GoogleOAuthResponse fetchToken(@RequestParam final MultiValueMap<String, String> params);
+
+  @GetExchange(url = "https://www.googleapis.com/oauth2/v1/userinfo")
+  GoogleMemberProfileResponse fetchMemberProfile(
+      @RequestHeader(name = AUTHORIZATION) String bearerToken
+  );
 }

befa82065c41ed6aa31066ba058228878064be20
hong-sile <hyukjoon.dev@gmail.com>
2024-03-10 16:54:45 +0900

    feat: Autorization Code로 로그인하는 기능 추가
    #1

--- src/main/java/capstone/safeat/member/controller/MemberController.java
+++ src/main/java/capstone/safeat/member/controller/MemberController.java
@@ -1,11 +1,11 @@
 package capstone.safeat.member.controller;
 
+import capstone.safeat.member.application.MemberService;
 import capstone.safeat.member.dto.LoginResponse;
-import capstone.safeat.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ public class MemberController {
 
   private final MemberService memberService;
 
-  @GetMapping("/login/oauth/{oauthType}")
+  @PostMapping("/login/oauth/{oauthType}")
   public ResponseEntity<LoginResponse> login(
       @PathVariable final String oauthType, @RequestParam final String code
   ) {

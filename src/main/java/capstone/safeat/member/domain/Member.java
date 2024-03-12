a8616e3e9b17c7eaa40a8259e9578448d9197299
hong-sile <hyukjoon.dev@gmail.com>
2024-03-12 23:41:48 +0900

    fix: equals and hashcode 업데이트
    #4

--- src/main/java/capstone/safeat/member/domain/Member.java
+++ src/main/java/capstone/safeat/member/domain/Member.java
@@ -1,5 +1,6 @@
 package capstone.safeat.member.domain;
 
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -20,5 +21,25 @@ public class Member {
   @Builder
   public Member(final String nickName, final String profileImageUrl) {
     this(null, nickName, profileImageUrl);
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final Member member = (Member) o;
+    if (member.id == null || id == null) {
+      return false;
+    }
+    return Objects.equals(id, member.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
   }
 }

4db48c4e6f4afc516cecfd6822ff501c16d87d97
hong-sile <hyukjoon.dev@gmail.com>
2024-04-12 17:28:20 +0900

    feat: 카테고리 전체조회 기능 추가
    #20

--- src/main/java/capstone/safeat/category/application/CategoryService.java
+++ src/main/java/capstone/safeat/category/application/CategoryService.java
@@ -4,6 +4,7 @@ import capstone.safeat.category.domain.Category;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,6 +12,7 @@ public class CategoryService {
 
   private final CategoryReader categoryReader;
 
+  @Transactional(readOnly = true)
   public List<Category> findAllCategory() {
     return categoryReader.readAll();
   }

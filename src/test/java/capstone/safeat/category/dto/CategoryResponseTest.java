c4cb8522629adcbbbdc2491309bf03d96b969f4a
hong-sile <hyukjoon.dev@gmail.com>
2024-04-20 20:43:58 +0900

    test: 요구사항에 맞는 테스트 작성

--- src/test/java/capstone/safeat/category/dto/CategoryResponseTest.java
+++ src/test/java/capstone/safeat/category/dto/CategoryResponseTest.java
@@ -12,20 +12,24 @@ class CategoryResponseTest {
   @Test
   void 카테고리들을_계층형으로_변환한다() {
     final List<CategoryResponse> expected = List.of(
-        new CategoryResponse(1L, "Fruit", "과일", List.of(
-            new CategoryResponse(3L, "Apple", "사과", List.of()),
-            new CategoryResponse(4L, "Mango", "망고", List.of())
-        )),
-        new CategoryResponse(2L, "Vegetables", "채소", List.of(
-            new CategoryResponse(5L, "Root Vegetables", "뿌리채소", List.of(
-                new CategoryResponse(7L, "Potato", "감자", List.of()),
-                new CategoryResponse(8L, "Sweet Potato", "고구마", List.of())
+        new CategoryResponse(1L, "Fruit", "과일", List.of(3L, 4L),
+            List.of(
+                new CategoryResponse(3L, "Apple", "사과", List.of(), List.of()),
+                new CategoryResponse(4L, "Mango", "망고", List.of(), List.of())
             )),
-            new CategoryResponse(6L, "Fruiting Vegetables", "과일채소", List.of(
-                new CategoryResponse(9L, "Cucumber", "오이", List.of()),
-                new CategoryResponse(10L, "Pepper", "고추", List.of())
+        new CategoryResponse(2L, "Vegetables", "채소", List.of(5L, 6L, 7L, 8L, 9L, 10L),
+            List.of(
+                new CategoryResponse(5L, "Root Vegetables", "뿌리채소", List.of(7L, 8L),
+                    List.of(
+                        new CategoryResponse(7L, "Potato", "감자", List.of(), List.of()),
+                        new CategoryResponse(8L, "Sweet Potato", "고구마", List.of(), List.of())
+                    )),
+                new CategoryResponse(6L, "Fruiting Vegetables", "과일채소", List.of(9L, 10L),
+                    List.of(
+                        new CategoryResponse(9L, "Cucumber", "오이", List.of(), List.of()),
+                        new CategoryResponse(10L, "Pepper", "고추", List.of(), List.of())
+                    ))
             ))
-        ))
     );
     final List<Category> categories = 카테고리들();
 

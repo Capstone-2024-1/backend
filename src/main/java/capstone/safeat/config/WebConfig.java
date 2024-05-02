4bbdf196d686530549045ef6e6babb5a0975d651
hong-sile <hyukjoon.dev@gmail.com>
2024-05-03 01:33:57 +0900

    feat: 임의로 CORS 전부 허용(#47)

---
+++ src/main/java/capstone/safeat/config/WebConfig.java
@@ -0,0 +1,18 @@
+package capstone.safeat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(final CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+        .allowCredentials(true)
+        .maxAge(3600);
+  }
+}

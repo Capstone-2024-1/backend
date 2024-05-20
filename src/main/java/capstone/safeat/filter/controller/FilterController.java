1903c1874d596e25d0de6299e4fee47f7f55f5c9
hong-sile <hyukjoon.dev@gmail.com>
2024-05-20 16:38:40 +0900

    hotfix: api endpoint 수정

--- src/main/java/capstone/safeat/filter/controller/FilterController.java
+++ src/main/java/capstone/safeat/filter/controller/FilterController.java
@@ -6,7 +6,6 @@ import capstone.safeat.filter.dto.FoodFilterResponse;
 import capstone.safeat.member.dto.JwtMemberId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/capstone/safeat/filter/external/FaissApiClient.java
+++ src/main/java/capstone/safeat/filter/external/FaissApiClient.java
@@ -6,6 +6,9 @@ import org.springframework.web.service.annotation.GetExchange;
 
 public interface FaissApiClient {
 
-  @GetExchange(url = "https://safeat2.dongwoo.win/search")
+  @GetExchange(url = "http://192.168.5.99/search")
   FoodEstimateResponse estimate(@RequestParam final MultiValueMap<String, String> params);
+
+  @GetExchange(url = "http://192.168.5.99/inference")
+  FoodEstimateResponse inference(@RequestParam final MultiValueMap<String, String> params);
 }

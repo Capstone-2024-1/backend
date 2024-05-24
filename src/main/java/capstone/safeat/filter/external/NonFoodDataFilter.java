4add26e94c90bd7249dfe28ec19a4da2806f4821
hong-sile <hyukjoon.dev@gmail.com>
2024-05-24 16:44:19 +0900

    feat: 한글이름인 경우만 반환하도록 수정(#87)

--- src/main/java/capstone/safeat/filter/external/NonFoodDataFilter.java
+++ src/main/java/capstone/safeat/filter/external/NonFoodDataFilter.java
@@ -5,10 +5,14 @@ public class NonFoodDataFilter {
   private static final String KOREAN_FORMAT_REGEX = "[가-힣]+";
 
   public static boolean isValidFoodName(final String foodName) {
-    return containOnlyKorean(foodName);
+    return containOnlyKorean(foodName) && containNonFoodKeyWord(foodName);
   }
 
   private static boolean containOnlyKorean(final String foodName) {
     return foodName.matches(KOREAN_FORMAT_REGEX);
+  }
+
+  private static boolean containNonFoodKeyWord(final String foodName) {
+    return true;
   }
 }

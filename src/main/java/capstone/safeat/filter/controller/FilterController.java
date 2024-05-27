9acac98ba955a9469e715ddc5d27b99e0bca0ed4
hong-sile <hyukjoon.dev@gmail.com>
2024-05-27 16:41:31 +0900

    feat: group으로 메뉴판 재구성하는 API 추가(#91)

--- src/main/java/capstone/safeat/filter/controller/FilterController.java
+++ src/main/java/capstone/safeat/filter/controller/FilterController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -31,11 +32,21 @@ public class FilterController {
   }
 
   @PostMapping(path = "/menu", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  public ResponseEntity<List<FoodFilterResponse>> estimateMultiFood(
+  public ResponseEntity<List<FoodFilterResponse>> generateMenuBoardForSingle(
       @RequestPart final MultipartFile menuImage, final JwtMemberId jwtMemberId
   ) {
     final List<FoodFilterResponse> foodFilterResponses
         = filterService.filterMultiFoodBy(menuImage, jwtMemberId.memberId());
+    return ResponseEntity.ok(foodFilterResponses);
+  }
+
+  @PostMapping(path = "/menu/groups/{groupId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<List<FoodFilterResponse>> generateMenuBoardForGroup(
+      @RequestPart final MultipartFile menuImage, final JwtMemberId jwtMemberId,
+      @PathVariable final Long groupId
+  ) {
+    final List<FoodFilterResponse> foodFilterResponses
+        = filterService.filterMultiFoodBy(menuImage, jwtMemberId.memberId(), groupId);
     return ResponseEntity.ok(foodFilterResponses);
   }
 
